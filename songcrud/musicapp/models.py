99cbd0481be39fb3a2c5dda9a03eea1046f4747b
Giftvictor12 <106772081+Giftvictor12@users.noreply.github.com>
2022-10-29 08:11:12 +0100

    Update models.py

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 500)
+    last_name = models.CharField(max_length = 500)
+    age = models.IntegerField(default = None)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length = 500)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    Content = models.CharField(max_length = 3000)
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
+   
+    def __str__(self):
+        return self.Content
